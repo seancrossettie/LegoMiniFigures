d557ec1153a95a1ba86c10650b03d5521c833e9e
seancrossettie <seancrossettie@gmail.com>
2021-07-27 19:06:15 -0500

    added cowboy torso class

---
+++ Torso/CowboyTorso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMiniFigures.Torso
+{
+    class CowboyTorso : TorsoBase
+    {
+        public bool HasPullString { get; set; }
+        public bool HasHolster { get; set; }
+
+        public override void Greeting()
+        {
+            base.Greeting();
+            Console.WriteLine("Got me a Snake in my boot");
+        }
+
+        public override void Wiggle()
+        {
+            Console.WriteLine("Cowboy friendly reaches for their holster.");
+        }
+
+        public void LassoDance()
+        {
+            Console.WriteLine("Take a dance with my lasso.");
+        }
+    }
+}
